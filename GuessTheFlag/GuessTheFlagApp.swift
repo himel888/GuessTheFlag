7cc272abe593fd841d91abf88c61ef4fc5b9ed5c
Kazi Abdullah Al Mamun <kazi.a.mamun@gmail.com>
2021-05-18 15:06:58 +0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kazi Abdullah Al Mamun on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
